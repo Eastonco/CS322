8d34c8a0ffd22847d135dd6725cd66008ac2ea99
Blaine Hindman <blaine.hindman@wsu.edu>
2020-01-23 15:14:15 -0800

    New Commit

---
+++ Bouldering/Controllers/employeedatasController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bouldering.Models;
+
+namespace Bouldering.Controllers
+{
+    public class employeedatasController : Controller
+    {
+
+        private UserEntities db = new UserEntities();
+        // GET: employeedatas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: userdatas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: userdatas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "employeeID,firstName,lastName,employeeEmail")] employeedata employeedata)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employeedatas.Add(employeedata);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+
+            return View(employeedata);
+        }
+    }
+}
---
+++ Bouldering/Controllers/userdatasController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bouldering.Models;
+
+namespace Bouldering.Controllers
+{
+    public class userdatasController : Controller
+    {
+        private UserEntities1 db = new UserEntities1();
+
+        // GET: userdatas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: userdatas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: userdatas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userID,firstName,lastName,userEmail")] userdata userdata)
+        {
+            if (ModelState.IsValid)
+            {
+                db.userdatas.Add(userdata);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+
+            return View(userdata);
+        }
+
+    }
+}
