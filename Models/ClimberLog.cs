a5eda29cb92240afb89d66add3e352c5cc841e8f
blainehindman-eecs <60160835+blainehindman-eecs@users.noreply.github.com>
2020-04-29 14:47:47 -0700

    Update ClimberLog.cs
    Update

--- Models/ClimberLog.cs
+++ Models/ClimberLog.cs
@@ -1,4 +1,13 @@
-﻿//
-//
-//
-//
+
+namespace Bouldering_Company.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class userLog
+    {
+        public string userID { get; set; }
+        public string timeStamp { get; set; }
+        public string stampRecord { get; set; }
+    }
+}
