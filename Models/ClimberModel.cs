473af896dcf74cdec3d17fd714936a8338342350
blainehindman-eecs <60160835+blainehindman-eecs@users.noreply.github.com>
2020-04-29 14:47:02 -0700

    Update ClimberModel.cs
    Update

--- Models/ClimberModel.cs
+++ Models/ClimberModel.cs
@@ -1,4 +1,14 @@
-﻿//
-//
-//
-//
+
+namespace Bouldering_Company.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class userdata
+    {
+        public string userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userEmail { get; set; }
+    }
+}
