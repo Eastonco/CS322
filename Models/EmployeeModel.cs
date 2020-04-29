42dd2b211c766a7dcad5421ca37e87404f855de4
blainehindman-eecs <60160835+blainehindman-eecs@users.noreply.github.com>
2020-04-29 14:46:14 -0700

    Update EmployeeModel.cs
    Update

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,4 +1,14 @@
-﻿//
-//
-//
-//
+
+namespace Bouldering_Company.Models
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class employeedata
+    {
+        public string employeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string employeeEmail { get; set; }
+    }
+}
